81f9c825ea1d3d7792f1fd77bb577eb6ce05e8b4
TrustHaul <your@email.com>
2026-06-13 10:06:59 -0400

    Set website host redirect and disable stale cache

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,16 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 import { getRouter } from "./router";
 
+if (
+  window.location.hostname === "dc-drip.com" &&
+  !window.location.pathname.startsWith("/admin") &&
+  !window.location.pathname.startsWith("/stats")
+) {
+  window.location.replace(
+    `https://www.dc-drip.com${window.location.pathname}${window.location.search}${window.location.hash}`,
+  );
+}
+
 const root = document.getElementById("root");
 
 if (!root) {
